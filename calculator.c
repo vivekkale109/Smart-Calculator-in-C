a237810d09208c3b8d514883fb1741dd79a30c27
vivek <vivekkale109@gmail.com>
2026-03-28 21:34:23 +0530

    add the stacks

--- calculator.c
+++ calculator.c
@@ -5,3 +5,17 @@
 #include <string.h>
 
 #define MAX 100
+
+// ---------- STACKS ----------
+char opStack[MAX];
+int top = -1;
+
+double valStack[MAX];
+int valTop = -1;
+
+void pushOp(char op) { opStack[++top] = op; }
+char popOp() { return opStack[top--]; }
+char peekOp() { return opStack[top]; }
+
+void pushVal(double val) { valStack[++valTop] = val; }
+double popVal() { return valStack[valTop--]; }
